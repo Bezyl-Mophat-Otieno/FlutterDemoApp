e7c6aa42ef7b1eaf60e33ce583470bce1d95acc3
Bezylmophat_Otieno <bezylmophatotieno@gmail.com>
2023-02-09 12:44:51 +0000

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(home: Home()));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text('SOMA_LINK'),
+        centerTitle: true,
+        backgroundColor: Colors.green[600],
       ),
       body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+          child: Image.network(
+              'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1503023345310-bd7c1de61c7d%3Fixlib%3Drb-4.0.3%26ixid%3DMnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8%26w%3D1000%26q%3D80&imgrefurl=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fhuman&tbnid=Y8NKKCmLLDotWM&vet=12ahUKEwjBmPi0_oX9AhXsuScCHcIrCtUQMygEegUIARDmAQ..i&docid=ZaycYywhXLmIVM&w=1000&h=1250&q=images&ved=2ahUKEwjBmPi0_oX9AhXsuScCHcIrCtUQMygEegUIARDmAQ')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => print('Hello world'),
+        child: Text('+'),
+        backgroundColor: Colors.green[600],
       ),
     );
   }
